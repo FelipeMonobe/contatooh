857243964e0170e51432ab99ad43b2915b2fc264
XinubeApps <XinubeApps@users.noreply.github.com>
2015-03-27 17:20:16 -0300

    Update protractor.js

--- config/protractor.js
+++ config/protractor.js
@@ -6,9 +6,9 @@ exports.config = {
 		browser.driver.get('http://localhost:3000');
 		browser.driver.findElement(by.id('entrar')).click();
 		browser.driver.findElement(by.id('login_field'))
-		.sendKeys('xinube@gmail.com');
+		.sendKeys('oculto@email.com');
 		browser.driver.findElement(by.id('password'))
-		.sendKeys('][cavera123][');
+		.sendKeys('senha');
 		browser.driver.findElement(by.name('commit')).click();
 	}
 };
