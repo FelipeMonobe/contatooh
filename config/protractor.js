28c269f6627d80740f3fbb7eb8bedfb68488c958
FelipeMonobe <xinube@gmail.com>
2015-03-28 23:46:10 -0300

    protractor wait page load

--- config/protractor.js
+++ config/protractor.js
@@ -15,11 +15,18 @@ exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 	onPrepare: function () {
 		browser.driver.get('http://localhost:3000');
+
+		browser.driver.wait(function() {
+    		return browser.driver.findElement(by.id('entrar')).isDisplayed();
+		}, 30000);
+
 		browser.driver.findElement(by.id('entrar')).click();
 		browser.driver.findElement(by.id('login_field'))
-		.sendKeys(config.seleniumUser);
+		//.sendKeys(config.seleniumUser);
+		.sendKeys('XinubeApps');
 		browser.driver.findElement(by.id('password'))
-		.sendKeys(config.seleniumUserPassword);
+		//.sendKeys(config.seleniumUserPassword);
+		.sendKeys('cavera123');
 		browser.driver.findElement(by.name('commit')).click();
 	}
 };
